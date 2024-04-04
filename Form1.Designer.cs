f4473e74bea211956a01883ee25dc708d5098a0e
Cristiana <cristiana.dica@cncn.ro>
2024-04-04 18:16:29 +0300

    Prime Number projects

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace PrimeNumber
+{
+    partial class PrimeNumber
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.PrimeNumberTextBox = new System.Windows.Forms.TextBox();
+            this.historyLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.Location = new System.Drawing.Point(287, 225);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(200, 70);
+            this.CheckButton.TabIndex = 0;
+            this.CheckButton.Text = "CHECK";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // PrimeNumberTextBox
+            // 
+            this.PrimeNumberTextBox.Location = new System.Drawing.Point(335, 158);
+            this.PrimeNumberTextBox.Name = "PrimeNumberTextBox";
+            this.PrimeNumberTextBox.Size = new System.Drawing.Size(100, 22);
+            this.PrimeNumberTextBox.TabIndex = 1;
+            // 
+            // historyLabel
+            // 
+            this.historyLabel.AutoSize = true;
+            this.historyLabel.Location = new System.Drawing.Point(381, 112);
+            this.historyLabel.Name = "historyLabel";
+            this.historyLabel.Size = new System.Drawing.Size(0, 16);
+            this.historyLabel.TabIndex = 2;
+            // 
+            // PrimeNumber
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.historyLabel);
+            this.Controls.Add(this.PrimeNumberTextBox);
+            this.Controls.Add(this.CheckButton);
+            this.Name = "PrimeNumber";
+            this.Text = "PrimeNumber";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.TextBox PrimeNumberTextBox;
+        private System.Windows.Forms.Label historyLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeNumber
+{
+    public partial class PrimeNumber : Form
+    {
+        public PrimeNumber()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            bool isNumber = Int32.TryParse(PrimeNumberTextBox.Text, out result);
+            if (isNumber)
+            {
+                bool isPrime = true;
+                for (int i = 2; i <= result / 2; i++)
+                {
+                    if (result % i == 0)
+                        isPrime = false;
+                }
+                if (isPrime)
+                {
+                    this.BackColor = Color.Green;
+                    historyLabel.BorderStyle = BorderStyle.FixedSingle;
+                    historyLabel.Text = historyLabel.Text + " "+ PrimeNumberTextBox.Text;
+                    PrimeNumberTextBox.Text = string.Empty;
+                }
+                else
+                {
+                    this.BackColor = Color.Red;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("S-a introdus un text");
+            };
+        }
+    }
+}
